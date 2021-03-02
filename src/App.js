80e12ad87ad57ce708c3db3fca04b4409dacfc80
Nathan Fedzen <nfedzen@hotmail.com>
2021-03-02 11:30:18 -0700

    components added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import Header from './components/Header'
+import Shirts from './components/Shirts'
+import Pants from './components/Pants'
+import Shoes from './components/Shoes'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <Header></Header>
+      <section>
+        <Shirts />
+        <Pants />
+        <Shoes />
+      </section>
+    </main>
+      
+  )
+    
+
 }
 
 export default App;
---
+++ src/components/Pants.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Pants(){
+  return (
+    <div>
+      <button>⬅️</button>
+      <img src="https://www.duluthtrading.com/dw/image/v2/BBNM_PRD/on/demandware.static/-/Sites-dtc-master-catalog/default/dw1a49d044/images/large/84310_SMT.jpg?sw=600&sh=600&sm=fit"/>
+      <button>➡️</button>
+    </div>
+  )
+}
